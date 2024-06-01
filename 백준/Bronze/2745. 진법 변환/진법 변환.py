788f1bf6e5382c621230da0446744e52036c7e1d
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-01 10:44:01 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,20 @@
+alpha = ['A','B','C','D','E','F','G','H','I'
+    ,'J','K','L','M','N','O','P','Q','R'
+    ,'S','T','U','V','W','X','Y','Z']
+def to10jinsu(jinStr, jinNum):
+    global alpha
+    jinNum = int(jinNum)
+    l = len(jinStr)
+    res = 0
+    for c in jinStr:
+        if c.isdigit():
+            _10jin = int(c)
+        else:
+            _10jin = alpha.index(c) + 10
+        # print(f"{_10jin} * {jinNum}^({l-1}) / ",end='')
+        res += _10jin * jinNum**(l-1)
+        l -= 1
+    return res
+
+jinStr, jinNum = input().split()
+print(to10jinsu(jinStr, jinNum))
