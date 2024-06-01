9359ea4f057f318ebbdc14f6799beddf3d373d77
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-01 17:24:33 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,8 +1,7 @@
 alpha = ['A','B','C','D','E','F','G','H','I'
     ,'J','K','L','M','N','O','P','Q','R'
     ,'S','T','U','V','W','X','Y','Z']
-# -> index + 10
-# ex) '2CA' 32진수 to 10진수 -> (2 * 32^2) + (12 * 32^1) + (10 * 32^0) 
+
 def to10jinsu(jinStr, jinNum):
     global alpha
     jinNum = int(jinNum)
@@ -13,7 +12,6 @@ def to10jinsu(jinStr, jinNum):
             _10jin = int(c)
         else:
             _10jin = alpha.index(c) + 10
-        # print(f"{_10jin} * {jinNum}^({l-1}) / ",end='')
         res += _10jin * jinNum**(l-1)
         l -= 1
     return res
