58af47b63010aba5779999fbab045e7d38976b62
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-03 10:09:15 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,4 +1,3 @@
-# 행렬 a + 행렬 b => 행렬 c 리턴
 def matrixAdd(n, m, a, b):
     c = []
     for i in range(n):
@@ -7,7 +6,6 @@ def matrixAdd(n, m, a, b):
             c[i].append(a[i][j] + b[i][j])
     return c
 
-# 입력
 n, m= map(int, input().split())
 a = []
 b = []
@@ -16,7 +14,7 @@ for i in range(n):
 for i in range(n):
     b.append(list(map(int, input().split())))
 
-# 출력
 c = matrixAdd(n, m, a, b)
 for i in range(n):
     print(' '.join(list(map(str, c[i]))))
+
