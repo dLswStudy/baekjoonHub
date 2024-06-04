ef4b5e0cef2e5a8a6b8cc3fba0d046c275191d72
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-04 23:14:23 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 80 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -5,22 +5,18 @@ count = []
 for _ in range(N):
     original.append(input())
 
+boolTypeW = lambda i,j: (((i+j)%2 == 0 and original[i][j] == 'W')
+                             or ((i+j)%2 == 1 and original[i][j] == 'B'))
 for a in range(N-7):
     for b in range(M-7):
         index1 = 0
         index2 = 0
         for i in range(a, a+8):
             for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
-                        index2 += 1
+                if boolTypeW(i,j):
+                    index1 += 1
                 else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
-                        index2 += 1
+                    index2 += 1
         count.append(min(index1, index2))
 
 print(min(count))
