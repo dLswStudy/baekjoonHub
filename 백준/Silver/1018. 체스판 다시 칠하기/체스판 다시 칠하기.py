90699cd1dc9ba257f9a4d4f6db17c0ae4432046a
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-05 00:13:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,19 +1,32 @@
+# 1018번 체스판 다시 칠하기
+
+# 입력 ----------------------
 n, m = map(int, input().split())
 matrix = [list(input()) for _ in range(n)]
-count = []
 
-boolTypeW = lambda i,j: (((i+j)%2 == 0 and matrix[i][j] == 'W')
-                             or ((i+j)%2 == 1 and matrix[i][j] == 'B'))
-matrix = [
-    [('TW' if boolTypeW(i, j) else 'TB') for j in range(m)] 
-    for i in range(n)]
-for a in range(n-7):
-    for b in range(m-7):
-        cntTypeW = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if matrix[i][j] == 'TW':
-                    cntTypeW += 1
-        count.append(min(cntTypeW, 64-cntTypeW))
+# 풀이 ----------------------
+def cntToFix(matrix, n, m):
+    boolTypeW = lambda i,j: (((i+j)%2 == 0 and matrix[i][j] == 'W') or 
+                             ((i+j)%2 == 1 and matrix[i][j] == 'B'))
 
-print(min(count))
+    for i in range(n):
+        for j in range(m):
+            if boolTypeW(i,j):
+                matrix[i][j] = 'TW'
+            else:
+                matrix[i][j] = 'TB'
+    count = []
+    for startI in range(0, n-7):
+        for startJ in range(0, m-7):
+            cntTypeW = 0
+            for i in range(startI, startI + 8):
+                for j in range(startJ, startJ + 8):
+                    if matrix[i][j] == 'TW':
+                        cntTypeW += 1
+            count.append(min(cntTypeW, 64-cntTypeW))
+
+    return min(count)
+
+# 출력 ----------------------
+print(cntToFix(matrix, n, m))
+
