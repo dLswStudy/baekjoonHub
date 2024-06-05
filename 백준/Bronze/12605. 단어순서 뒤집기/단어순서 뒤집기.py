a7e5749ddb85d6c8dc256a0c14d1431fe022b98b
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-05 13:49:39 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,6 +1,10 @@
 n = int(input())
 cases = [input().split() for _ in range(n)]
 
-# 풀이 1: [::=1] 사용
+# 풀이 2: for문 사용
 for i in range(n):
-    print(f"Case #{i+1}: {' '.join(cases[i][::-1])}")
+    stack = []
+    m = len(cases[i])
+    for j in range(m):
+        stack.append(cases[i][m-1-j])
+    print(f"Case #{i+1}: {' '.join(stack)}")
