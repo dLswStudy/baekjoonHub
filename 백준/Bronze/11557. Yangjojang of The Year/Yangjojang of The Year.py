e693e28d1790b33d234d460b4e0cb7f4706048a5
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-07 14:46:21 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 44 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -0,0 +1,21 @@
+import sys
+from heapq import heappush
+
+input = sys.stdin.readline
+t = int(input())
+tCases = []
+# 케이스 별로 입력 & 힙푸시 & 정답도출
+for _ in range(t):
+    n = int(input())
+    amountDict = {}
+    maxHeap = []
+    for _ in range(n):
+        school, amount = input().split()
+        amount = int(amount)
+        heappush(maxHeap, -amount)
+        amountDict[amount] = school
+    maxAmt = -maxHeap[0]
+    maxSchool = amountDict[maxAmt]
+    tCases.append((maxAmt, maxSchool))
+for tCase in tCases:
+    print(tCase[1])
