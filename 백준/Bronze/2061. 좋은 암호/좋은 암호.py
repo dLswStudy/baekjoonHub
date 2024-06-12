12f02eeeaaaf3826ce9db5929cb0f56d6eb83bc7
dLswStudy <84223829+dLswStudy@users.noreply.github.com>
2024-06-12 20:12:29 +0900

    [Bronze III] Title: 좋은 암호, Time: 268 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2061. 좋은 암호/좋은 암호.py
+++ 백준/Bronze/2061. 좋은 암호/좋은 암호.py
@@ -1,12 +1,17 @@
-k, l = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-_1stFactor = -1
-for num in range(2, l):
-    if k % num == 0:
-        _1stFactor = num
+k, l = input().split()
+k = int(k)
+L = int(l)
+is_bad = False
+# 2부터 L까지 나누어 떨어진다면 (BAD + 인수) 출력 
+for i in range(2, L):
+    if k % i == 0:
+        print("BAD", i)
+        is_bad = True
         break
 
-if _1stFactor == -1:
-    print('GOOD')
-else:
-    print('BAD', _1stFactor)
+# 그 외에는 좋은 암호
+if not is_bad:
+    print("GOOD")
